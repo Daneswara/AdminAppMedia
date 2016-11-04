fd9d4d1ab3917f63184ab413320807fa1f994742
Daneswara <daneswarajauhari@gmail.com>
2016-11-04 11:03:17 +0700

    tambah ke firebase

--- dist/berita.php
+++ dist/berita.php
@@ -1,24 +1,56 @@
+<script src="https://www.gstatic.com/firebasejs/3.4.1/firebase.js"></script>
+<script src="js/fire.js">
+</script>
 <?php
-
+$awal = microtime(true);
 require 'simple_html_dom.php';
 
-// Create DOM from URL or file
-$html = file_get_html('http://filkom.ub.ac.id/');
+// Detik.com
+try {
+    $html = file_get_html('http://news.detik.com/');
+    $element = $html->find('a[data-action=Newsfeed / NHL]', 6);
+    $url = $element->href;
 
-// Find all images 
-$element = $html->find('div[class=col-md-6 col-md-pull-6]', 0);
-foreach ($element->find('div') as $berita) {
-    if ($berita->class == "cards-body") {
-        foreach ($berita->find('a') as $link) {
-            echo $link->href . '<br>';
-            konten($link->href);
-        }
-    }
+    $html = file_get_html('http:' . $element->href);
+//judul
+    $berita = $html->find('article', 0);
+    $judul = $berita->find('h1', 0)->plaintext;
+// gambar
+    $gambar = $berita->find('div[class=pic_artikel]', 0);
+    $image = $gambar->find('img', 0)->src;
+// berita
+    $isi = $berita->find('div[class=detail_text]', 0)->plaintext;
+    $isiberita = str_replace("
+", "<br>", $isi);
+
+    // tambah ke firebase
+    ?>
+    <script>
+        tambahBerita('Detik.com', '<?php echo $url ?>', '<?php echo $judul ?>', '<?php echo $image ?>', '<?php echo $isiberita ?>');
+    </script>
+    <?php
+} catch (Exception $e) {
+    echo 'Error Detik.com: ' . $e->getMessage();
 }
 
-function konten($url) {
-    $target = file_get_html($url);
-    echo $target->find('h1[class=title-content margin-top-no margin-bottom-no font-raleway font-orange padding-bottom-no]', 0)->plaintext;
-    echo "<br>";
-}
+
+/**
+  foreach ($element->find('div') as $berita) {
+  if ($berita->class == "cards-body") {
+  foreach ($berita->find('a') as $link) {
+  echo $link->href . '<br>';
+  konten($link->href);
+  }
+  }
+  }
+
+  function konten($url) {
+  $target = file_get_html($url);
+  echo $target->find('h1[class=title-content margin-top-no margin-bottom-no font-raleway font-orange padding-bottom-no]', 0)->plaintext;
+  echo "<br>";
+  }
+ * */
+$akhir = microtime(true);
+$lama = $akhir - $awal;
+echo "<p>Lama eksekusi script adalah: " . $lama . " detik</p>";
 ?>
--- dist/js/fire.js
+++ dist/js/fire.js
@@ -1,10 +1,41 @@
-(function() {
-    var config = {
-        apiKey: "AIzaSyAPGTn-2wJevQvOCH4E8cNQzJwdQmz7Zxk",
-        authDomain: "rssmedia-3300c.firebaseapp.com",
-        databaseURL: "https://rssmedia-3300c.firebaseio.com",
-        storageBucket: "rssmedia-3300c.appspot.com",
-        messagingSenderId: "675061780565"
-    };
-    firebase.initializeApp(config);
-}());
+
+var config = {
+    apiKey: "AIzaSyAPGTn-2wJevQvOCH4E8cNQzJwdQmz7Zxk",
+    authDomain: "rssmedia-3300c.firebaseapp.com",
+    databaseURL: "https://rssmedia-3300c.firebaseio.com",
+    storageBucket: "rssmedia-3300c.appspot.com",
+    messagingSenderId: "675061780565"
+};
+
+firebase.initializeApp(config);
+var waktu;
+function tambahBerita(sumber, link, judul, gambar, berita) {
+    var date = myFunction();
+    firebase.database().ref('berita/' + date).set({
+        tanggal: waktu,
+        sumber: sumber,
+        url: link,
+        judul: judul,
+        gambar: gambar,
+        berita: berita.trim()
+    });
+}
+
+function addZero(x, n) {
+    while (x.toString().length < n) {
+        x = "0" + x;
+    }
+    return x;
+}
+
+function myFunction() {
+    var d = new Date();
+    var tanggal = addZero(d.getDate(), 2);
+    var bulan = addZero(d.getMonth(), 2);
+    var tahun = addZero(d.getFullYear(), 4);
+    var h = addZero(d.getHours(), 2);
+    var m = addZero(d.getMinutes(), 2);
+    var s = addZero(d.getSeconds(), 2);
+    waktu = tahun+"-"+bulan+"-"+tanggal+" "+h+":"+m+":"+s;
+    return tahun+""+bulan+""+tanggal+""+h+""+m+""+s;
+}
